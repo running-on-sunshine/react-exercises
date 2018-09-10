48360f49a3c74bc68e8601ed1e50d22e8cb911e3
Liz Khuu <khuu.liz@gmail.com>
2018-09-10 16:00:49 -0400

    Create new component to display books

--- bookstore/main.js
+++ bookstore/main.js
@@ -5,12 +5,15 @@
 
 let h = React.createElement;
 
+let bookRow = (bookTitle) => 
+    h('li', {}, [bookTitle])
+
 let vdom = h('div', {}, [
     h('h1', { className: 'big-header' }, ['Bookstore']),
     h('ul', {}, [
-        h('li', {}, ['Book 1']),
-        h('li', {}, ['Book 2']),
-        h('li', {}, ['Book 3'])
+        bookRow('Book 1'),
+        bookRow('Book 2'),
+        bookRow('Book 3')
         ]),
     h('footer', {}, ['Copyright 2018']),
     h('a', { href: 'mypage.com' }, ['My Website']),
