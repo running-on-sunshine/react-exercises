8663363ff5b9016d7976f4ca308e44b0f423bef6
Liz Khuu <khuu.liz@gmail.com>
2018-09-11 13:30:04 -0400

    Create component for whole page

--- bookstore/main.js
+++ bookstore/main.js
@@ -5,27 +5,34 @@
 
 let h = React.createElement;
 
+let PageHeader = (props) =>
+    h('h1', { className: 'big-header' }, ['Bookstore'])
+
 let BookRow = (props) => 
     h('li', {}, [
         h('h2', {}, props.title),
         h('p', {}, 'Lorem ipsum!')
     ])
 
-let BookList = (props) => {
-    return h('ul', {}, 
+let BookList = (props) => 
+    h('ul', {}, 
         props.books.map(bookTitle => 
             h(BookRow, { title: bookTitle })
         )
-    );
-};
+    )
 
-let vdom = h('div', {}, [
-    h('h1', { className: 'big-header' }, ['Bookstore']),
-    h(BookList, { books: ['Book 1', 'Book 2', 'Book 3'] }),
-    h(BookList, { books: ['A Tale of Two Cities', 'Book 7', 'Book 12'] }),
-    h('footer', {}, ['Copyright 2018']),
-    h('a', { href: 'mypage.com' }, ['My Website']),
-    ]
-);
+let PageFooter = (props) =>
+    h('div', {}, [
+        h('footer', {}, ['Copyright 2018']),
+        h('a', { href: 'mypage.com' }, ['My Website'])
+    ])
 
-ReactDOM.render(vdom, document.querySelector('.react-root'));
+let Homepage = (props) =>
+    h('div', {}, [
+        h(PageHeader, {}),
+        h(BookList, { books: ['Book 1', 'Book 2', 'Book 3'] }),
+        h(BookList, { books: ['A Tale of Two Cities', 'Book 7', 'Book 12'] }),
+        h(PageFooter, {})
+    ])
+
+ReactDOM.render(h(Homepage), document.querySelector('.react-root'));
