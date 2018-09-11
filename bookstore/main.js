2b791f3c5e52fd15c45e2937e8a964dff80dfbe7
Liz Khuu <khuu.liz@gmail.com>
2018-09-11 09:16:58 -0400

    Capitalized component name

--- bookstore/main.js
+++ bookstore/main.js
@@ -5,15 +5,15 @@
 
 let h = React.createElement;
 
-let bookRow = (bookTitle) => 
+let BookRow = (bookTitle) => 
     h('li', {}, [bookTitle])
 
 let vdom = h('div', {}, [
     h('h1', { className: 'big-header' }, ['Bookstore']),
     h('ul', {}, [
-        bookRow('Book 1'),
-        bookRow('Book 2'),
-        bookRow('Book 3')
+        BookRow('Book 1'),
+        BookRow('Book 2'),
+        BookRow('Book 3')
         ]),
     h('footer', {}, ['Copyright 2018']),
     h('a', { href: 'mypage.com' }, ['My Website']),
